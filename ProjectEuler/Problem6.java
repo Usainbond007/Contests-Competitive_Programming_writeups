25f2d8376c61655c272bf749afb35aa629c2a1fc
Ishan Chowdhury <10.ishanc@gmail.com>
2026-03-15 19:03:18 +0530

    Update Problem6.java

--- ProjectEuler/Problem6.java
+++ ProjectEuler/Problem6.java
@@ -1,1 +1,12 @@
-
+public class Problem6 {
+    public static void main(String[] args) {
+        int n = 100;
+        long s = 0;
+        long sq = 0;
+        for (int i = 1; i <= n; i++) {
+            s += i;
+            sq += i * i;
+        }
+        long res = s * s - sq;
+        System.out.println(res);
+}}
