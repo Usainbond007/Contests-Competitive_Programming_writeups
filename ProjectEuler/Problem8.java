6a251e465e97dd152b079464a433c48d3487aa8b
Ishan Chowdhury <10.ishanc@gmail.com>
2026-03-15 19:11:35 +0530

    Create Problem8.java

---
+++ ProjectEuler/Problem8.java
@@ -0,0 +1,16 @@
+public class Problem8 {
+    public static void main(String[] args) {
+        long num = 20;
+        while (true) {
+            boolean divisible = true;
+            for (int i = 1; i <= 20; i++) {
+                if (num % i != 0) {
+                    divisible = false;
+                    break;
+                }}
+            if (divisible) {
+                System.out.println(num);
+                break;
+            }
+            num += 20;
+        }}}
