3f41f0e203b36be6d31e6b50a74cea13137d5f01
Ishan Chowdhury <10.ishanc@gmail.com>
2026-03-15 17:28:10 +0530

    Implement minCost method in Solution class

---
+++ LeetCode/Weekly_Contests/Weekly_Contest_491/q2.java
@@ -0,0 +1,12 @@
+class Solution
+{
+    public int minCost(int n) 
+    {
+        return n*(n-1)/2;
+    }
+}
+/*
+basically 1+2+3+..n-1
+so basic summation
+https://leetcode.com/problems/minimum-cost-to-split-into-ones/
+*/
