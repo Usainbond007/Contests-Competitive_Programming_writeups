3c4cf8600f054a80771ad377a6a21405b33e2b93
Ishan Chowdhury <10.ishanc@gmail.com>
2026-03-15 18:49:12 +0530

    Create Problem3.java

---
+++ ProjectEuler/Problem3.java
@@ -0,0 +1,15 @@
+
+public class Problem3 {
+    public static void main(String[] args) {
+
+        long n = 600851475143L;
+        long factor = 2;
+        while (factor * factor <= n) {
+            if (n % factor == 0)
+                n /= factor;
+            else
+                factor++;
+        }
+        System.out.println(n);
+    }
+}
